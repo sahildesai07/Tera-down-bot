1568e6aea1c929709dd3b7f4cc7ef297191babe9
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-15 23:00:14 -0700

    Create database.py

---
+++ database/database.py
@@ -0,0 +1,45 @@
+# 
+
+import motor.motor_asyncio
+from config import DB_URI, DB_NAME
+
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
+database = dbclient[DB_NAME]
+
+user_data = database['users']
+
+default_verify = {
+    'is_verified': False,
+    'verified_time': 0,
+    'verify_token': "",
+    'link': ""
+}
+
+def new_user(id):
+    return {
+        '_id': id,
+        'verify_status': {
+            'is_verified': False,
+            'verified_time': 0,
+            'verify_token': "",
+            'link': ""
+        }
+    }
+
+async def present_user(user_id: int):
+    found = await user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    user = new_user(user_id)
+    await user_data.insert_one(user)
+    return
+
+async def db_verify_status(user_id):
+    user = await user_data.find_one({'_id': user_id})
+    if user:
+        return user.get('verify_status', default_verify)
+    return default_verify
+
+async def db_update_verify_status(user_id, verify):
+    await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
