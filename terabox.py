c4be7c613a76d3f8dafd1b055281d757496ba384
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-18 20:15:46 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -10,67 +10,51 @@ import os
 import time
 from status import format_progress_bar
 from video import download_video, upload_video
-from pymongo import MongoClient
+from web import keep_alive
 
 load_dotenv('config.env', override=True)
+
 logging.basicConfig(level=logging.INFO)
 
-api_id = os.environ.get('TELEGRAM_API', 22505271)
+api_id = os.environ.get('TELEGRAM_API', '22421139')
 if len(api_id) == 0:
     logging.error("TELEGRAM_API variable is missing! Exiting now")
     exit(1)
 
-api_hash = os.environ.get('TELEGRAM_HASH', 'c89a94fcfda4bc06524d0903977fc81e')
+api_hash = os.environ.get('TELEGRAM_HASH', 'aec62a51ce5c4b106a336d81b20e9c05')
 if len(api_hash) == 0:
     logging.error("TELEGRAM_HASH variable is missing! Exiting now")
     exit(1)
     
-bot_token = os.environ.get('BOT_TOKEN', '7156255687:AAEXQtlTzE8Jbwt9VD6NLfcZX08Czu7w7gQ')
+bot_token = os.environ.get('BOT_TOKEN', '6319534565:AAGgx7JzkFzrVLNprOKqHBuQoIQN69CQfcc')
 if len(bot_token) == 0:
     logging.error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
-dump_id = os.environ.get('DUMP_CHAT_ID', '-1002062925443')
+dump_id = os.environ.get('DUMP_CHAT_ID', '-1002222899728')
 if len(dump_id) == 0:
     logging.error("DUMP_CHAT_ID variable is missing! Exiting now")
     exit(1)
 else:
     dump_id = int(dump_id)
 
-fsub_id = os.environ.get('FSUB_ID', '-1002108419450')
+fsub_id = os.environ.get('FSUB_ID', '-1002178412016')
 if len(fsub_id) == 0:
     logging.error("FSUB_ID variable is missing! Exiting now")
     exit(1)
 else:
     fsub_id = int(fsub_id)
 
-# MongoDB setup
-mongo_url = os.environ.get('MONGO_URL', 'mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-client = MongoClient(mongo_url)
-db = client['uphdlust']
-users_collection = db['users']
-
-def save_user(user_id, username):
-    if users_collection.find_one({'user_id': user_id}) is None:
-        users_collection.insert_one({'user_id': user_id, 'username': username})
-        logging.info(f"Saved new user {username} with ID {user_id} to the database.")
-    else:
-        logging.info(f"User {username} with ID {user_id} is already in the database.")
-
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.command("start"))
 async def start_command(client, message):
-    user_id = message.from_user.id
-    username = message.from_user.username
-    save_user(user_id, username)  # Save user to MongoDB
-
     sticker_message = await message.reply_sticker("CAACAgIAAxkBAAEYonplzwrczhVu3I6HqPBzro3L2JU6YAACvAUAAj-VzAoTSKpoG9FPRjQE")
     await asyncio.sleep(2)
     await sticker_message.delete()
     user_mention = message.from_user.mention
     reply_message = f"ᴡᴇʟᴄᴏᴍᴇ, {user_mention}.\n\n🌟 ɪ ᴀᴍ ᴀ ᴛᴇʀᴀʙᴏx ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ʙᴏᴛ. sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴛᴇʀᴀʙᴏx ʟɪɴᴋ ɪ ᴡɪʟʟ ᴅᴏᴡɴʟᴏᴀᴅ ᴡɪᴛʜɪɴ ғᴇᴡ sᴇᴄᴏɴᴅs ᴀɴᴅ sᴇɴᴅ ɪᴛ ᴛᴏ ʏᴏᴜ ✨."
-    join_button = InlineKeyboardButton("ᴊᴏɪɴ ❤️🚀", url="https://t.me/ultroid_official")
-    developer_button = InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ⚡️", url="https://t.me/ultroidxTeam")
+    join_button = InlineKeyboardButton("ᴊᴏɪɴ ❤️🚀", url="https://t.me/+jQCpu-i2i5BmN2Qx")
+    developer_button = InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ⚡️", url="https://t.me/Nfc_nothing")
     reply_markup = InlineKeyboardMarkup([[join_button, developer_button]])
     await message.reply_text(reply_message, reply_markup=reply_markup)
 
@@ -89,14 +73,11 @@ async def is_user_member(client, user_id):
 @app.on_message(filters.text)
 async def handle_message(client, message: Message):
     user_id = message.from_user.id
-    username = message.from_user.username
-    save_user(user_id, username)  # Save user to MongoDB
-
     user_mention = message.from_user.mention
     is_member = await is_user_member(client, user_id)
 
     if not is_member:
-        join_button = InlineKeyboardButton("ᴊᴏɪɴ ❤️🚀", url="https://t.me/ultroid_official")
+        join_button = InlineKeyboardButton("ᴊᴏɪɴ ❤️🚀", url="https://t.me/+jQCpu-i2i5BmN2Qx")
         reply_markup = InlineKeyboardMarkup([[join_button]])
         await message.reply_text("ʏᴏᴜ ᴍᴜsᴛ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ.", reply_markup=reply_markup)
         return
@@ -113,27 +94,8 @@ async def handle_message(client, message: Message):
         await upload_video(client, file_path, thumbnail_path, video_title, reply_msg, dump_id, user_mention, user_id, message)
     except Exception as e:
         logging.error(f"Error handling message: {e}")
-        await reply_msg.edit_text("ғᴀɪʟᴇᴅ ᴛᴏ ᴘʀᴏᴄᴇss ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ.\nɪғ ʏᴏᴜʀ ғɪʟᴇ sɪᴢᴇ ɪs ᴍᴏʀᴇ ᴛʜᴀɴ 120ᴍʙ ɪᴛ ᴍɪɢʜᴛ ғᴀɪʟ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ.")
-
-@app.on_message(filters.command("broadcast") & filters.user(6695586027))  # Replace <your_user_id> with your actual user ID to restrict this command
-async def broadcast_command(client, message):
-    if len(message.command) < 2:
-        await message.reply_text("Please provide a message to broadcast.")
-        return
-
-    broadcast_message = message.text.split(maxsplit=1)[1]  # Get the message to broadcast
-
-    users = users_collection.find()
-    for user in users:
-        user_id = user['user_id']
-        try:
-            await client.send_message(user_id, broadcast_message)
-            logging.info(f"Broadcast message sent to user {user_id}.")
-        except Exception as e:
-            logging.error(f"Failed to send broadcast message to user {user_id}: {e}")
-            continue
-
-    await message.reply_text("Broadcast message sent to all users.")
+        await reply_msg.edit_text("ғᴀɪʟᴇᴅ ᴛᴏ ᴘʀᴏᴄᴇss ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ.\nɪғ ʏᴏᴜʀ ғɪʟᴇ sɪᴢᴇ ɪs ᴍᴏʀᴇ ᴛʜᴀɴ 120ᴍʙ ɪᴛ ᴍɪɢʜᴛ ғᴀɪʟ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ.\nᴛʜɪs ɪs ᴛʜᴇ ᴛᴇʀᴀʙᴏx ɪssᴜᴇ, sᴏᴍᴇ ʟɪɴᴋs ᴀʀᴇ ʙʀᴏᴋᴇɴ, sᴏ ᴅᴏɴᴛ ᴄᴏɴᴛᴀᴄᴛ ʙᴏᴛ's ᴏᴡɴᴇʀ")
 
 if __name__ == "__main__":
+    keep_alive()
     app.run()
