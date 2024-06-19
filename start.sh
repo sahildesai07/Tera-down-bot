4157592ea5c7b0ab3f23bb23bea5437c9b13d0c6
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-19 19:49:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-echo "Starting aria2c..."
-aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon
-echo "Waiting for aria2c to start..."
-sleep 5  # Wait for 5 seconds
-echo "aria2c started, now starting Python script..."
-python terabox.py
+aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon && python3 terabox.py
