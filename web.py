efdaa6a1b8720a219d5cad541df882dcc47f16cc
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-23 01:23:32 -0700

    Update web.py

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running"
+    return "Bot is running...t.me/ultroidxTeam"
 
 def run():
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
