5e2a3dc2c93eaa2fa739c59f4235a62670f0f519
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-25 03:01:18 -0700

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "1b69cabcad8a473ca707823d783a0f9ccf7c3583")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/HTDTeraBox/7") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/howto_openurllink") # shareus ka tut_vid he 
