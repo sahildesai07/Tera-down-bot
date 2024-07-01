d8036cc5b333468232dc14d1601510bbe8001a94
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-07-01 18:28:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "aphdlust")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+sxxxxtroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "aphdlxxust")
 
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
@@ -13,4 +13,4 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "1b69cabcad8a473ca707823d783a0f9ccf7c3583")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/howto_openurllink") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/howto_oxxxnurllink") # shareus ka tut_vid he 
