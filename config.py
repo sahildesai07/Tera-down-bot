0a8b99253b4ba68697a47640c963af553677b9a7
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-07-11 09:08:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "uphdlxxust")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hegodal811:rsRu17pspZAcp6V7@cluster0.prsvqax.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "just2earn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "7e100dd62679b6fc9aea48ea106347edad521d7f")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "ziplinker.net")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "4b884da539a2f5d579e2a6f805e623c7a082a3b1")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/Digital_Heaven_Team/61") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/Toturial_Crezy/99") # shareus ka tut_vid he 
