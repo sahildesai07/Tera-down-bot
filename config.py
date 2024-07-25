d706f827ee1953ecf452822836dc817662219acf
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-07-25 10:31:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "ziplinker.net")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "4b884da539a2f5d579e2a6f805e623c7a082a3b1")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/Toturial_Crezy/99") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
