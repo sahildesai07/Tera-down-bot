4f38c689a02960dbf55271225bf701ee29e39e5f
opabhik <abhishekkr05012007@gmail.com>
2024-12-30 16:00:02 +0530

    Update video.py

--- video.py
+++ video.py
@@ -15,7 +15,7 @@ aria2 = aria2p.API(
     )
 )
 async def download_video(url, reply_msg, user_mention, user_id):
-    response = requests.get(f"https://terabox.udayscriptsx.workers.dev/?url={url}")
+    response = requests.get(f"https://true12g.in/api/terabox.php?url={url}")
     response.raise_for_status()
     data = response.json()
 
