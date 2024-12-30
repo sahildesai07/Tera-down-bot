b5650aa538e5bf8f09d20b82aeba75dc3e29e147
opabhik <abhishekkr05012007@gmail.com>
2024-12-30 15:29:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 # check my discription to help by using my refer link of shareus.io
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "ziplinker.net")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "4b884da539a2f5d579e2a6f805e623c7a082a3b1")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "link4earn.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "065980d01c653f8ddd5c9722c9becaf68dfc4d27")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
